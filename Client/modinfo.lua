6590780cd6928fec2d236fb854543e530ab3cc0e
AceHanded <vili.kononen@gmail.com>
2024-05-12 14:32:02 +0300

    Add project files

---
+++ Client/modinfo.lua
@@ -0,0 +1,23 @@
+-- All the basic information
+name = "Finnish Translation Pack"
+description = "Älä näänny, yhdessä!\n\n"..
+              "Suomenkielinen kielipaketti Don't Starve Togetheriin, joka tähtää (jokseenkin) äärimmäiseen tarkkuuteen ja alkuperäisen tekstityylin myötäilyyn.\n\n"..
+              "Nauttikaa!"
+author = "AceHand"
+version = "0.5.1"
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+api_version = 10
+
+-- Tags to find servers using this mod
+server_filter_tags = {"suomi", "suomalainen", "suomenkielinen", "kieli", "finland", "finnish", "language"}
+
+-- Compatibleness
+dont_starve_compatible=false
+reign_of_giants_compatible=false
+shipwrecked_compatible=false
+dst_compatible=true
+all_clients_require_mod=false
+
+-- Set to 'true' for the client-sided mod, and 'false' for the server-sided one (due to character lines)
+client_only_mod=true
---
+++ Server/modmain.lua
@@ -0,0 +1,1 @@
+LoadPOFile("finnish.po", "fi")
